c4fdd3e588d5dabc131a938168d0ee80c35eca15
ichetov <ichetowwwkin@mail.ru>
2021-06-07 03:10:30 +0300

    added New Design, Our Insights & Articles

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,29 @@ dots: true,
 arrows: false,
 speed: 1200,
 autoplay: true,
-})
 
-var mixer = mixitup('.products-week__content');
+});
+
+$('.partners__wrap').slick({
+dots: true,
+arrows: false,
+speed: 1200,
+});
+
+
+
+
+
+var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+var mixer = mixitup(containerEl1, config);
+var mixer = mixitup(containerEl2, config);
   
 
 });
