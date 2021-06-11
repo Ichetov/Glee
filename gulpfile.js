37f83695ae8c9d66d507501a9bcbad23e35b155f
ichetov <ichetowwwkin@mail.ru>
2021-06-12 00:37:32 +0300

    fixed bugs

--- gulpfile.js
+++ gulpfile.js
@@ -73,8 +73,10 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
+   
   ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
